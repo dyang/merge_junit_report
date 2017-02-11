06199e30d762ca7a4e7b3fc32e72c82d5de17b2a
Derek Yang <yanghada@gmail.com>
2017-02-11 21:03:06 +0100

    Tweaking codeclimate coverage config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 # This module is only used to check the environment is currently a testing env
@@ -7,7 +10,5 @@ end
 require 'fastlane' # to import the Action super class
 require 'fastlane/plugin/merge_junit_report' # import the actual plugin
 require 'codeclimate-test-reporter'
-require 'simplecov'
 
-SimpleCov.start
 Fastlane.load_actions # load other actions (in case your plugin calls other actions or shared values)
